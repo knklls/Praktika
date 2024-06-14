a848b97749579146838f03380bf3dd975aa3a765
knklls <knklls88@gmail.com>
2024-06-14 23:25:27 +0400

    Delete pr4 directory

--- pr4/src/name.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function MyComponent() {
-   return <h1>Анастасия</h1>
-}
-
-export default MyComponent;
--- pr4/src/polzovatel.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function User(props) {
-  return <h1>Привет, я {props.name}, мне {props.age} лет, я живу {props.city}</h1>
-}
-
-export default User
